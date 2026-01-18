3f4ec82faaed4fbff1dfc7be40a14ef99efe1445
VysakhSdev <vysakhs807@gmail.com>
2026-01-18 18:18:09 +0530

    bug fixed in uploads

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,8 +1,11 @@
 import multer from 'multer';
 import path from 'path';
 
+const __dirname = path.resolve();
+
 const storage = multer.diskStorage({
-  destination: './uploads',
+  
+  destination: `${__dirname}/uploads`,
   filename: (req, file, cb) => {
     cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
   }
